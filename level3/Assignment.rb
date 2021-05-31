2b398529785fc2fa613d9bfbd622bf5e55eec3c3
ShubhankSinghal <shubhank.singhal98@gmail.com>
2021-05-31 09:25:45 +0530

    level3 assignment ready

--- level3/Assignment.rb
+++ level3/Assignment.rb
@@ -1,62 +1,89 @@
 require "date"
 
 class Todo
+
+  # Initializing todo item
   def initialize(text, due_date, completed)
     @text = text
     @due_date = due_date
     @completed = completed
   end
 
+  # getter for text
+  def text?
+    @text
+  end
+
+  # getter for due_date
+  def due_date?
+    @due_date
+  end
+
+  # getter for completed
+  def completed?
+    @completed
+  end
+
+  # to fetch overdue
   def overdue?
-    @due_date < Date.today
+    due_date? < Date.today
   end
 
+  # to fetch due_today
   def due_today?
-    @due_date == Date.today
+    due_date? == Date.today
   end
 
+  # to fetch due_later
   def due_later?
-    @due_date > Date.today
+    due_date? > Date.today
   end
 
+  # to format data into required string
   def to_displayable_string
     str = ""
-    if @completed
-      str = "[X] #{@text}"
+    if completed?
+      str = "[X] #{text?}"
     else
-      str = "[ ] #{@text}"
+      str = "[ ] #{text?}"
     end
-    if @due_date != Date.today
-      str = "#{str} #{@due_date}"
+    if due_date? != Date.today
+      str = "#{str} #{due_date?}"
     end
     return str
   end
 end
 
 class TodosList
+
+  # initialize the todoslist
   def initialize(todos)
     @todos = todos
   end
 
+  # fetch the overdue list
   def overdue
     TodosList.new(@todos.filter { |todo| todo.overdue? })
   end
 
+  # fetch the due_today list
   def due_today
     TodosList.new(@todos.filter { |todo| todo.due_today? })
   end
 
+  # fetch the due_later list
   def due_later
     TodosList.new(@todos.filter { |todo| todo.due_later? })
   end
 
+  # to add data into todolist
   def add(data)
     @todos << data
   end
 
+  # transform the data
   def to_displayable_list
     @todos.map { |x| x.to_displayable_string }
-    @todos.join("\n")
   end
 end
 
